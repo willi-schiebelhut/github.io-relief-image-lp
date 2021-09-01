8024bdc9b766a75834dd5546f63f78f988dcdd22
Willie <shibelgut.dev@gmail.com>
2021-09-01 22:42:33 +0600

    add responcive version

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,39 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const swiper = new Swiper('.feedbacks__slider', {
+        // Optional parameters
+        // loop: true,
+        slidesPerView: 3,
+        spaceBetween: 25,
+        grabCursor: true,
+        // If we need pagination
+        pagination: {
+          el: '.slider__pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+            },
+            992: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+        }
+      });
 });
